ffcfe6181e941c3c7e50103f4773fa233fbb8bcc
slledru <slledru@msn.com>
2018-04-14 16:30:48 -0600

    additional npm packages and boilerplate source files

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const boom = require('boom')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index')
+const stravaRouter = require('./routes/strava')
+const loginRouter = require('./routes/login')
 
-var app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/login', loginRouter)
+app.use('/strava', stravaRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use((req, res, next) => {
+  next(boom.notFound())
+})
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+app.use((err, req, res, next) => {
+  console.log(err)
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.output.statusCode || 500)
+  res.json(err.output.payload)
+})
 
-module.exports = app;
+module.exports = app
---
+++ knexfile.js
@@ -0,0 +1,37 @@
+'use strict'
+const path = require('path')
+
+module.exports = {
+  development: {
+    client: 'pg',
+    migrations: {
+      directory: path.join(__dirname, 'database', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'database', 'seeds')
+    },
+    connection: 'postgres://localhost/bike_health_dev'
+  },
+
+  test: {
+    client: 'pg',
+    migrations: {
+      directory: path.join(__dirname, 'database', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'database', 'seeds')
+    },
+    connection: 'postgres://localhost/bike_health_test'
+  },
+
+  production: {
+    client: 'pg',
+    migrations: {
+      directory: path.join(__dirname, 'database', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'database', 'seeds')
+    },
+    connection: process.env.DATABASE_URL
+  }
+}
