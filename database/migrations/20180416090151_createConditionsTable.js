2ca4a9667cd579acdeaa892ebe49ae29525baef2
slledru <slledru@msn.com>
2018-04-16 10:19:01 -0600

    added conditions migration

--- database/migrations/20180416090151_createConditionsTable.js
+++ database/migrations/20180416090151_createConditionsTable.js
@@ -1,8 +1,22 @@
+const { conditionsTable, ridesTable } = require('../utils')
 
-exports.up = function(knex, Promise) {
-  
-};
+/*
+  type: weather, road
+  condition for weather: sunny, rain, snow
+  condition for road: dry, wet, icy
+*/
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable(conditionsTable, (table) => {
+    table.increments()
+    table.varchar('type', 20).notNullable()
+    table.varchar('condition', 20).notNullable()
+    table.integer('ride_id').notNullable()
+    table.foreign('ride_id').references('id').inTable(ridesTable).onDelete('cascade')
+    table.timestamps(true, true)
+  })
+}
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable(conditionsTable)
+}
