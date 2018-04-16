16dcfb4d9929005834c272c32dd87806de35add3
slledru <slledru@msn.com>
2018-04-16 10:08:48 -0600

    added repairs migration

--- database/migrations/20180416090144_createRepairsTable.js
+++ database/migrations/20180416090144_createRepairsTable.js
@@ -1,8 +1,21 @@
+const { bikesTable, partsTable, repairsTable } = require('../utils')
 
-exports.up = function(knex, Promise) {
-  
-};
+/*
+  type: repair, replace, clean
+*/
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable(repairsTable, (table) => {
+    table.increments()
+    table.integer('bike_id').notNullable()
+    table.foreign('bike_id').references('id').inTable(bikesTable).onDelete('cascade')
+    table.integer('part_id').notNullable()
+    table.foreign('part_id').references('id').inTable(partsTable).onDelete('cascade')
+    table.varchar('type', 20).notNullable()
+    table.timestamps(true, true)
+  })
+}
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable(repairsTable)
+}
