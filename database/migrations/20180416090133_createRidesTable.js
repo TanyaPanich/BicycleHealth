4970f3d38c05a57cf6e037e9c4a6b07666a1bd09
slledru <slledru@msn.com>
2018-04-16 10:14:53 -0600

    added rides migration

--- database/migrations/20180416090133_createRidesTable.js
+++ database/migrations/20180416090133_createRidesTable.js
@@ -1,8 +1,22 @@
+const { bikesTable, ridesTable } = require('../utils')
 
-exports.up = function(knex, Promise) {
-  
-};
+/*
+  distance_unit: mile, km
+*/
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable(ridesTable, (table) => {
+    table.increments()
+    table.varchar('strava_ride_id', 255).notNullable().defaultTo('')
+    table.dateTime('rode_at').notNullable().defaultTo(knex.raw('now()'))
+    table.integer('distance').notNullable().defaultTo(0)
+    table.varchar('distance_unit', 20).notNullable()
+    table.integer('bike_id').notNullable()
+    table.foreign('bike_id').references('id').inTable(bikesTable).onDelete('cascade')
+    table.timestamps(true, true)
+  })
+}
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable(ridesTable)
+}
