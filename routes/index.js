0a9d731e0ca12845b2cfa49e0d55fc8f84a1aa85
Tanya <mellene@gmail.com>
2018-04-16 11:32:16 -0600

    all menu views

--- routes/index.js
+++ routes/index.js
@@ -19,9 +19,24 @@ const addRide = (req, res, next) => {
   res.render('addRide', { title: 'Bicycle health' })
 }
 
+const settings = (req, res, next) => {
+  res.render('settings', { title: 'Bicycle health' })
+}
+
+const profile = (req, res, next) => {
+  res.render('profile', { title: 'Bicycle health' })
+}
+
+const history = (req, res, next) => {
+  res.render('history', { title: 'Bicycle health' })
+}
+
 router.get('/', index)
 router.get('/bicycle', addBicycle)
 router.get('/part', addPart)
 router.get('/ride', addRide)
+router.get('/settings', settings)
+router.get('/profile', profile)
+router.get('/history', history)
 
 module.exports = router
