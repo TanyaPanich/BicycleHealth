2a8f5c5d4627166d090704a412908cc5522fe98e
Tanya <mellene@gmail.com>
2018-04-15 23:10:05 -0600

    added bike part ride views and routes

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,25 @@ const router = express.Router()
 
 /* GET home page. */
 
-router.get('/', (req, res, next) => {
+const index = (req, res, next) => {
   res.render('index', { title: 'Bicycle health' })
-})
+}
+
+const addBicycle = (req, res, next) => {
+  res.render('addBicycle', { title: 'Bicycle health' })
+}
+
+const addPart = (req, res, next) => {
+  res.render('addPart', { title: 'Bicycle health' })
+}
+
+const addRide = (req, res, next) => {
+  res.render('addRide', { title: 'Bicycle health' })
+}
+
+router.get('/', index)
+router.get('/bicycle', addBicycle)
+router.get('/part', addPart)
+router.get('/ride', addRide)
 
 module.exports = router
--- routes/loginSignup.js
+++ routes/loginSignup.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
+const loginSignup = (req, res, next) => {
   res.render('loginSignup', { title: 'Bicycle health' })
-})
+}
+
+const signup = (req, res, next) => {
+  res.render('signup', { title: 'Bicycle health' })
+}
+
+const login = (req, res, next) => {
+  res.render('login', { title: 'Bicycle health' })
+}
+
+router.get('/', loginSignup)
+router.get('/signup', signup)
+router.get('/login', login)
 
 module.exports = router
