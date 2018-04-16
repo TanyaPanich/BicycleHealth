1d7b1284b09927453a2ba643e3bb73dcd023c25c
slledru <slledru@msn.com>
2018-04-16 14:51:14 -0600

    boilerplate for services code

---
+++ database/services/rideService.js
@@ -0,0 +1,17 @@
+const knex = require('../../knex')
+const boom = require('boom')
+const {
+  usersTable,
+  bikesTable,
+  partsTable,
+  ridesTable,
+  repairsTable,
+  conditionsTable
+} = require('../utils')
+
+class RideService {
+}
+
+module.exports = {
+
+}
---
+++ database/services/teamService.js
@@ -0,0 +1,12 @@
+const knex = require('../../knex')
+const boom = require('boom')
+const {
+  teamsTable
+} = require('../utils')
+
+class TeamService {
+}
+
+module.exports = {
+
+}
