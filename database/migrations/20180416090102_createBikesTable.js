c0ac5afcd0eaa94efcfee7eac09844837109961f
slledru <slledru@msn.com>
2018-04-16 09:59:14 -0600

    added bikes migration

--- database/migrations/20180416090102_createBikesTable.js
+++ database/migrations/20180416090102_createBikesTable.js
@@ -1,8 +1,25 @@
+const { usersTable, bikesTable } = require('../utils')
 
-exports.up = function(knex, Promise) {
-  
-};
+/*
+  type: road_bike, mountain_bike, city_bike
+  distance_unit: mile, km
+*/
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable(bikesTable, (table) => {
+    table.increments()
+    table.varchar('nick_name', 255).notNullable()
+    table.varchar('type', 20).notNullable()
+    table.varchar('brand', 255).notNullable().defaultTo('')
+    table.varchar('model', 255).notNullable().defaultTo('')
+    table.integer('distance').notNullable().defaultTo(0)
+    table.varchar('distance_unit', 20).notNullable()
+    table.integer('user_id').notNullable()
+    table.foreign('user_id').references('id').inTable(usersTable).onDelete('cascade')
+    table.timestamps(true, true)
+  })
+}
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable(bikesTable)
+}
