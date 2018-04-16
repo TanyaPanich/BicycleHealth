551fa9f9deaa1dab983c5bdb8c558b617e381eb9
slledru <slledru@msn.com>
2018-04-16 10:05:34 -0600

    added parts migration

--- database/migrations/20180416090112_createPartsTable.js
+++ database/migrations/20180416090112_createPartsTable.js
@@ -1,8 +1,24 @@
+const { bikesTable, partsTable } = require('../utils')
 
-exports.up = function(knex, Promise) {
-  
-};
+/*
+  unit: mile, km
+*/
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable(partsTable, (table) => {
+    table.increments()
+    table.varchar('name', 255).notNullable()
+    table.integer('bike_id').notNullable()
+    table.foreign('bike_id').references('id').inTable(bikesTable).onDelete('cascade')
+    table.varchar('brand', 255).notNullable().defaultTo('')
+    table.varchar('model', 255).notNullable().defaultTo('')
+    table.integer('max_life_span').notNullable().defaultTo(100)
+    table.integer('distance').notNullable().defaultTo(0)
+    table.varchar('unit', 20).notNullable()
+    table.timestamps(true, true)
+  })
+}
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable(partsTable)
+}
