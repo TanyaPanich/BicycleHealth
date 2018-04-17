9314b65f286c2b396bfdd5a9cff275c6597bd31a
slledru <slledru@msn.com>
2018-04-17 17:02:48 -0600

    added seed for user

--- database/seeds_dev/002_insertUsers.js
+++ database/seeds_dev/002_insertUsers.js
@@ -5,6 +5,7 @@ exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
   const userUuid1 = uuid()
   const userUuid2 = uuid()
+  const userUuid3 = uuid()
   const defaultTeamId = initializeDefaultTeamId()
   return knex(usersTable).del()
     .then(() => {
@@ -26,6 +27,16 @@ exports.seed = (knex, Promise) => {
           last_name: 'Doe',
           email: 'john.doe@abc.com',
           access_type: 'normal',
+          team_id: defaultTeamId,
+          created_at: new Date('2018-04-16 15:26:16 UTC'),
+          updated_at: new Date('2018-04-16 15:26:16 UTC')
+        },
+        {
+          id: userUuid3,
+          first_name: 'Pascal',
+          last_name: 'Ledru',
+          email: 'pascal.ledru@gmail.com',
+          access_type: 'strava',
           team_id: defaultTeamId,
           created_at: new Date('2018-04-16 15:26:16 UTC'),
           updated_at: new Date('2018-04-16 15:26:16 UTC')
