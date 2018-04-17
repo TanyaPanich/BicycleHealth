6bc36ca01d43683edbd6eb54c9bcb9510da1a759
slledru <slledru@msn.com>
2018-04-16 19:43:39 -0600

    added ride services

--- database/services/rideService.js
+++ database/services/rideService.js
@@ -11,8 +11,109 @@ const {
 } = require('../utils')
 
 class RideService {
+  list(bikeId) {
+    return knex(ridesTable)
+      .where('bike_id', id)
+      .catch((err) => {
+        console.log('list: err', err)
+        throw boom.badImplementation(`Error retrieving rides by bicycle id, ${id}`)
+      })
+  }
+
+  get(id) {
+    return knex(ridesTable)
+      .where('id', id)
+      .then((rows) => {
+        if (rows.length === 1) {
+          return rows[0]
+        }
+        if (rows.length > 1) {
+          throw boom.badImplementation(`Too many rides for the id, ${id}`)
+        }
+        throw boom.notFound(`No rides found for the id, ${id}`)
+      })
+      .catch((err) => {
+        console.log('get: err', err)
+        throw boom.badImplementation(`Error retrieving ride with the id, ${id}`)
+      })
+  }
+
+  insert(ride) {
+    if (!ride.bike_id) {
+      throw boom.badRequest('Bike id is required')
+    }
+    if (!ride.distance_unit) {
+      throw boom.badRequest('Distance unit is required')
+    }
+    return knex(ridesTable)
+      .insert({
+        id: uuid(),
+        strava_ride_id: ride.strava_ride_id,
+        bike_id: ride.bike_id,
+        rode_at: ride.rode_at,
+        distance: ride.distance,
+        distance_unit: ride.distance_unit
+      })
+      .returning('*')
+      .then((rows) => {
+        if (rows.length === 1) {
+          return rows[0]
+        }
+        if (rows.length > 1) {
+          throw boom.badImplementation(`Too many rides for the id, ${rows[0].id}`)
+        }
+        throw boom.badImplementation(`Unable to insert ride`)
+      })
+      .catch((err) => {
+        console.log('insert: err', err)
+        throw boom.badImplementation(`Error inserting ride for bike, ${ride.bike_id}`)
+      })
+  }
+
+  update(ride) {
+    if (!ride.id) {
+      throw boom.badRequest('Id is required')
+    }
+    return knex(ridesTable)
+      .update({
+        strava_ride_id: ride.strava_ride_id,
+        rode_at: ride.rode_at,
+        distance: ride.distance,
+        distance_unit: ride.distance_unit
+      })
+      .where('id', ride.id)
+      .then((rows) => {
+        if (rows.length === 0) {
+          return rows[0]
+        }
+        if (rows.length > 1) {
+          throw boom.badImplementation(`Too many rides for the id, ${rows[0].id}`)
+        }
+        throw boom.badImplementation(`Unable to update ride`)
+      })
+      .catch((err) => {
+        console.log('update: err', err)
+        throw boom.badImplementation(`Error updating ride for id, ${ride.id}`)
+      })
+  }
+
+  delete(id) {
+    return knex(ridesTable)
+      .where('id', id)
+      .then((rows) => {
+        if (rows.length === 1) {
+          return rows[0]
+        }
+        if (rows.length > 1) {
+          throw boom.badImplementation(`Too many rides for the id, ${id}`)
+        }
+        throw boom.notFound(`No ride found for the id, ${id}`)
+      })
+      .catch((err) => {
+        console.log('delete: err', err)
+        throw boom.badImplementation(`Error deleting ride with the id, ${id}`)
+      })
+  }
 }
 
-module.exports = {
-
-}
+module.exports = RideService
