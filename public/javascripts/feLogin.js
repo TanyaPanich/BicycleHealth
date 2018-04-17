2ec40d5660a3c233e3b24e72260ab646a10cf27c
Tanya <mellene@gmail.com>
2018-04-16 23:54:16 -0600

    signup and login work

---
+++ public/javascripts/feLogin.js
@@ -0,0 +1,37 @@
+$('#login-form').submit((event) => {
+  event.preventDefault()
+  $('#emailStatus').empty()
+  $('#passwordStatus').empty()
+
+  const email = $('#InputLoginEmail').val().trim()
+  const password = $('#InputLoginPassword').val()
+
+  if (!email) {
+    $('#emailStatus').append('Email must not be blank')
+    return
+  }
+
+  if (email.indexOf('@') < 0) {
+    $('#emailStatus').append('Email must be valid')
+    return
+  }
+
+  if (!password || password.length < 3) {
+    $('#passwordStatus').append('Password must be at least 8 characters long')
+    return
+  }
+
+  const options = {
+    data: { email, password },
+    type: 'POST',
+    url: '/login/login'
+  };
+
+  $.ajax(options)
+    .done(() => {
+      window.location.href = '/'
+    })
+    .fail(($xhr) => {
+      console.log('error happened', $xhr)
+    });
+})
--- routes/loginSignup.js
+++ routes/loginSignup.js
@@ -5,71 +5,92 @@ const bcrypt = require('bcrypt')
 const boom = require('boom')
 const jwt = require('jsonwebtoken')
 const uuid = require('uuid/v4')
-const { defaultTeamId } = require('../database/utils')
 const TeamService = require('../database/services/teamService')
-
+const UserService = require('../database/services/userService')
 
 const loginSignup = (req, res, next) => {
-  res.render('loginSignup', { title: 'Bicycle health' })
-}
-
-const signup = (req, res, next) => {
-  res.render('signup', { title: 'Bicycle health' })
-}
-
-const login = (req, res, next) => {
-  res.render('login', { title: 'Bicycle health' })
-}
-
-const insert = (req, res, next) => {
-  //const teamService = new TeamService()
-
-  console.log("I am on insert!", req.body)
-  if(!req.body.email) next(boom.badRequest('Email must not be blank'))
-  isEmailExist(req.body.email, next)
-  if(!req.body.password1 || req.body.password1.length < 3)
-    next(boom.badRequest('Password must be at least 8 characters long'))
-
-  knex('users')
-    .returning(['id','first_name','last_name', 'email'])
-    .insert({
-        id : uuid(),
-        first_name : req.body.firstName,
-        last_name : req.body.lastName,
-        email : req.body.email,
-        access_type : 'normal',
-        team_id : '9318a7bf-faa7-47bf-9d60-fd5c4ed7ac39',
-        // team_id : teamService.getDefault(),
-        hashed_password : bcrypt.hashSync(req.body.password1, 8)
-    }).then((user) => {
-      console.log("User successfully inserted", user)
-
-      const token = jwt.sign({'email': req.body.email }, process.env.SECRET_KEY)
-      res.setHeader('Set-Cookie', `token=${token};`)
-      res.render('index', { title: 'Bicycle health' })
-    }).catch(err => {
-      console.log("Failed to insert User", req.body.email)
-      console.log("ERR in insert:", err)
-
-        next(err)
-    })
-}
-
-function isEmailExist(email, next) {
-  knex('users')
-  .then(data => {
-    for(let user of data) {
-      if(user.email === email) {
-        return next(boom.badRequest('Email already exists'))
-      }
-    }
+  res.render('loginSignup', {
+    title: 'Bicycle health'
   })
 }
 
+const signup = (req, res, next) => {
+  res.render('signup', {
+    title: 'Bicycle health'
+  })
+}
+
+const login = (req, res, next) => {
+  res.render('login', {
+    title: 'Bicycle health'
+  })
+}
+
+const handleResponse = (op, email, httpRes, response) => {
+  return response.then(user => {
+    console.log(`${op} succeeded for user ${email}:`, user)
+    const token = jwt.sign({
+      'email': email
+    }, process.env.SECRET_KEY)
+    httpRes.setHeader('Set-Cookie', `token=${token};`)
+    httpRes.render('index', {
+      title: 'Bicycle health'
+    })
+  }).catch(err => {
+    console.log(`${op} failed for user ${email}: ${err}`)
+    throw err
+  })
+}
+
+const signupPost = (req, res, next) => {
+  console.log("signupPost:", req.body)
+
+  const teamService = new TeamService()
+  const userService = new UserService()
+  return handleResponse(
+    'signup',
+    req.body.email,
+    res,
+    teamService.getDefault().then(teamId => {
+      const inputUser = {
+        first_name: req.body.firstName,
+        last_name: req.body.lastName,
+        email: req.body.email,
+        hashed_password: bcrypt.hashSync(req.body.password1, 8),
+        access_type: 'normal',
+        team_id: teamId.id
+      }
+      console.log("team_id:", inputUser.team_id)
+      return userService.insert(inputUser)
+    })
+  ).catch(err => {
+    next(err)
+  })
+}
+
+const loginPost = (req, res, next) => {
+  console.log("loginPost:", req.body)
+  const userService = new UserService()
+  return handleResponse(
+    'login',
+    req.body.email,
+    res,
+    userService.getByEmail(req.body.email)
+    .then(user => bcrypt.compare(req.body.password, user.hashed_password)
+    .then(match => {
+      console.log("loginPost bcrypt:", match,user)
+      if (!match) throw boom.unauthorized('invalid email or password')
+      return user
+    }))
+  ).catch(err => {
+    next(err)
+  })
+}
 
 router.get('/', loginSignup)
 router.get('/signup', signup)
-router.post('/signup', insert)
+router.post('/signup', signupPost)
 router.get('/login', login)
+router.post('/login', loginPost)
 
 module.exports = router
