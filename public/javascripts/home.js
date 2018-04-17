74d45bf15ba8a2df349521ce4e37ff80d4671a7d
Mac Decker <mcdecke@gmail.com>
2018-04-17 15:06:57 -0600

    phaser writing to parent page

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -6,32 +6,26 @@ var GameState = {
     this.load.image('bike', '../images/bike-vector-full.png')
     this.load.image('wheel', '../images/wheel.png')
     this.load.image('brake', '../images/disc-brake-logo.png')
+
+    this.load.image('seat', '../images/seat.png')
+    this.load.image('chain', '../images/chain.png')
+    this.load.image('pedal', '../images/pedal.png')
+    this.load.image('handlebars', '../images/handlebars.png')
+    this.load.image('chainring', '../images/chainring.png')
   },
   //executed after everything is loaded
   create: function() {
 
     console.log('hello strava');
-    // consider refactoring all these create statements
-    // make a function that takes the x, y, and image name
-    // and does the pixelPerfectClick, ect... in it.
-    // Also write a func for setting/getting the customParams
-
-    // need to use local storage to keep last clicked bike part,
-    // and read from there to rewrite it to the db.
-
-
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
     this.scale.pageAlignHorizontally = true
     this.scale.pageAlignVertically = true
-
     this.background = this.game.add.sprite(0, 0, 'background')
+
 
     //load bike
     this.bike = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'bike')
     this.bike.anchor.setTo(0.5)
-    this.bike.inputEnabled = true;
-    this.bike.input.pixelPerfectClick = true;
-
     //stats will be pulled in from db and written here
     this.bike.customParams = {
       health: 99,
@@ -55,7 +49,6 @@ var GameState = {
     this.brake = this.game.add.sprite(this.game.world.centerX - 184, this.game.world.centerY + 66, 'brake')
     this.brake.anchor.setTo(0.5)
     this.brake.scale.setTo(0.04)
-
     //stats will be pulled in from db and written here
     this.brake.customParams = {
       health: 100,
@@ -64,20 +57,74 @@ var GameState = {
     }
     this.start(this.brake, 'input', this.brake)
 
+    //load handlebars
+    this.handlebars = this.game.add.sprite(this.game.world.centerX + 104, this.game.world.centerY - 154, 'handlebars')
+    this.handlebars.anchor.setTo(0.5)
+    //stats will be pulled in from db and written here
+    this.handlebars.customParams = {
+      health: 100,
+      distance: 1500,
+      lifespan: 15000
+    }
+    this.start(this.handlebars, 'input', this.handlebars)
+
+    //load seat
+    this.seat = this.game.add.sprite(this.game.world.centerX - 104, this.game.world.centerY - 150, 'seat')
+    this.seat.anchor.setTo(0.5)
+    //stats will be pulled in from db and written here
+    this.seat.customParams = {
+      health: 11,
+      distance: 1500,
+      lifespan: 15000
+    }
+    this.start(this.seat, 'input', this.seat)
+
+    //load chain
+    this.chain = this.game.add.sprite(this.game.world.centerX - 124, this.game.world.centerY + 84, 'chain')
+    this.chain.anchor.setTo(0.5)
+    //stats will be pulled in from db and written here
+    this.chain.customParams = {
+      health: 100,
+      distance: 15000,
+      lifespan: 15000
+    }
+    this.start(this.chain, 'input', this.chain)
+
+    //load pedal
+    this.pedal = this.game.add.sprite(this.game.world.centerX - 6, this.game.world.centerY + 144, 'pedal')
+    this.pedal.anchor.setTo(0.5)
+    //stats will be pulled in from db and written here
+    this.pedal.customParams = {
+      health: 11,
+      distance: 1500,
+      lifespan: 15000
+    }
+    this.start(this.pedal, 'input', this.pedal)
+
+    //load chainring
+    this.chainring = this.game.add.sprite(this.game.world.centerX - 36, this.game.world.centerY + 90, 'chainring')
+    this.chainring.anchor.setTo(0.5)
+    //stats will be pulled in from db and written here
+    this.chainring.customParams = {
+      health: 11,
+      distance: 1500,
+      lifespan: 15000
+    }
+    this.start(this.chainring, 'input', this.chainring)
+
     //adds back arrow
     this.arrow = this.game.add.sprite(this.game.world.centerX - 330, this.game.world.centerY - 200, 'arrow')
     this.arrow.anchor.setTo(0.5)
     this.arrow.scale.x = -1
-    backText = game.add.text(0, 0, "Back!", this.arrow);
-    backText.addColor('#ffffff')
-    backText.anchor.set(0.5);
+    backText = game.add.text(0, 0, "Back!", this.arrow)
+    backText.anchor.set(0.5)
     this.start(this.arrow, 'back', this.arrow)
 
     //adds fixed button
     this.fixed = this.game.add.sprite(this.game.world.centerX + 330, this.game.world.centerY - 200, 'arrow')
     this.fixed.anchor.setTo(0.5)
     fixedText = game.add.text(0, 0, "Fixed!", this.fixed);
-    fixedText.anchor.set(0.5);
+    fixedText.anchor.set(0.5)
     this.start(this.fixed, 'fixed', this.fixed)
   },
 
@@ -85,40 +132,52 @@ var GameState = {
     //wheels spinnin'
     this.wheel.angle += .5
     //text sticks to arrow
-    backText.x = Math.floor(this.arrow.x + 8);
-    backText.y = Math.floor(this.arrow.y + 2);
+    backText.x = Math.floor(this.arrow.x + 8)
+    backText.y = Math.floor(this.arrow.y + 2)
 
-    fixedText.x = Math.floor(this.fixed.x - 8);
-    fixedText.y = Math.floor(this.fixed.y + 2);
-
-
+    fixedText.x = Math.floor(this.fixed.x - 8)
+    fixedText.y = Math.floor(this.fixed.y + 2)
   },
 
   writeBikeStats: function(part) {
-    console.log(part);
-
     let obj = {
       'distance': part.customParams.distance,
       'health': part.customParams.health,
-      'lifespan':part.customParams.lifespan,
-      name:part.key
+      'lifespan': part.customParams.lifespan,
+      name: part.key
     }
 
-    hpLabel = this.game.add.text(this.game.world.centerX - 150, 20, `Health: ${obj.health}`, {
-      font: '24px Arial',
-      fill: '#000'
-    })
-    distLabel = this.game.add.text(this.game.world.centerX + 50, 60, `Distance: ${obj.distance}`, {
-      font: '24px Arial',
-      fill: '#000'
-    })
-    lifespanLabel = this.game.add.text(this.game.world.centerX - 150, 60, `Wear: ${(obj.distance / obj.lifespan)*100}  %`, {
-      font: '24px Arial',
-      fill: '#000'
-    })
+    // hpLabel = this.game.add.text(this.game.world.centerX - 150, 20, `Health: ${obj.health}`, {
+    //   font: '24px Arial',
+    //   fill: '#000'
+    // })
+    // distLabel = this.game.add.text(this.game.world.centerX + 50, 60, `Distance: ${obj.distance}`, {
+    //   font: '24px Arial',
+    //   fill: '#000'
+    // })
+    // lifespanLabel = this.game.add.text(this.game.world.centerX - 150, 60, `Wear: ${(obj.distance / obj.lifespan)*100}  %`, {
+    //   font: '24px Arial',
+    //   fill: '#000'
+    // })
 
     localStorage.setItem('partObj', JSON.stringify(obj))
 
+    this.getBikeStats()
+
+  },
+
+  getBikeStats: function() {
+
+        let partObj = JSON.parse(localStorage.getItem('partObj'))
+
+        console.log(partObj.health)
+        console.log(partObj.distance)
+        console.log(partObj.lifespan)
+        console.log(partObj.name)
+        // //write new stats to db
+        const parts = parent.document.getElementById('partHealth')
+        console.log(parts);
+        $(parts).text(`Distance since ${partObj.name} were changed: ` + partObj.distance) ` km`;
   },
 
   fixBikeStats: function() {
@@ -128,10 +187,14 @@ var GameState = {
     partObj.distance = 0
     partObj.health = 100
 
-    console.log(partObj)
-    //write new stats to db
-
-
+    console.log(partObj.health)
+    console.log(partObj.distance)
+    console.log(partObj.lifespan)
+    console.log(partObj.name)
+    // //write new stats to db
+    const parts = parent.document.getElementById('partHealth')
+    console.log(parts);
+    $(parts).text(`Distance since ${partObj.name} were changed: ` + partObj.distance) ` km`;
   },
 
   back: function() {
@@ -152,5 +215,5 @@ var GameState = {
 
 let game = new Phaser.Game(800, 600, Phaser.AUTO)
 
-game.state.add('GameState', GameState);
-game.state.start('GameState');
+game.state.add('GameState', GameState)
+game.state.start('GameState')
