d05222a003e7f8a5fa9d26a5406345e15f259417
Mac Decker <mcdecke@gmail.com>
2018-04-17 09:08:20 -0600

    phaser?

--- routes/home.js
+++ routes/home.js
@@ -1,17 +1,23 @@
-//this game will have only 1 state
 var GameState = {
-  //load the game public before the game starts
+  //load the game assets before the game starts
   preload: function() {
-    this.load.image('background', 'public/images/background-gradient.svg')
-    this.load.image('arrow', 'public/images/arrow.png')
-    this.load.image('bike', 'public/images/bike-vector-full.png')
-    this.load.image('wheel', 'public/images/wheel.png')
-    this.load.image('brake', 'public/images/disc-brake-logo.png')
+    this.load.image('background', 'assets/images/background-gradient.svg')
+    this.load.image('arrow', 'assets/images/arrow.png')
+    this.load.image('bike', 'assets/images/bike-vector-full.png')
+    this.load.image('wheel', 'assets/images/wheel.png')
+    this.load.image('brake', 'assets/images/disc-brake-logo.png')
   },
   //executed after everything is loaded
   create: function() {
 
-    const calledOnce = false;
+    // consider refactoring all these create statements
+    // make a function that takes the x, y, and image name
+    // and does the pixelPerfectClick, ect... in it.
+    // Also write a func for setting/getting the customParams
+
+    // need to use local storage to keep last clicked bike part,
+    // and read from there to rewrite it to the db.
+
 
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
     this.scale.pageAlignHorizontally = true
@@ -24,79 +30,120 @@ var GameState = {
     this.bike.anchor.setTo(0.5)
     this.bike.inputEnabled = true;
     this.bike.input.pixelPerfectClick = true;
+
+    //stats will be pulled in from db and written here
     this.bike.customParams = {
       health: 99,
       distance: 1200,
       lifespan: 5000
     }
-    this.startInput(this.bike)
+    this.start(this.bike, 'input', this.bike)
 
     //load wheel
     this.wheel = this.game.add.sprite(this.game.world.centerX + 183, this.game.world.centerY + 66, 'wheel')
     this.wheel.anchor.setTo(0.5)
+    //stats will be pulled in from db and written here
     this.wheel.customParams = {
       health: 10,
       distance: 600,
-      lifespan: 5000
+      lifespan: 500
     }
-    this.startInput(this.wheel)
+    this.start(this.wheel, 'input', this.wheel)
 
     //load brake
     this.brake = this.game.add.sprite(this.game.world.centerX - 184, this.game.world.centerY + 66, 'brake')
     this.brake.anchor.setTo(0.5)
     this.brake.scale.setTo(0.04)
+
+    //stats will be pulled in from db and written here
     this.brake.customParams = {
       health: 100,
       distance: 0,
       lifespan: 5000
     }
-    this.startInput(this.brake)
+    this.start(this.brake, 'input', this.brake)
 
-    this.arrow = this.game.add.sprite(this.game.world.centerX - 350, this.game.world.centerY - 200, 'arrow')
+    //adds back arrow
+    this.arrow = this.game.add.sprite(this.game.world.centerX - 330, this.game.world.centerY - 200, 'arrow')
     this.arrow.anchor.setTo(0.5)
     this.arrow.scale.x = -1
-    this.startBack(this.arrow)
+    backText = game.add.text(0, 0, "Back!", this.arrow);
+    backText.anchor.set(0.5);
+    this.start(this.arrow, 'back', this.arrow)
 
+    //adds fixed button
+    this.fixed = this.game.add.sprite(this.game.world.centerX + 330, this.game.world.centerY - 200, 'arrow')
+    this.fixed.anchor.setTo(0.5)
+    fixedText = game.add.text(0, 0, "Fixed!", this.fixed);
+    fixedText.anchor.set(0.5);
+    this.start(this.fixed, 'fixed', this.fixed)
   },
 
   update: function() {
     //wheels spinnin'
     this.wheel.angle += .5
+    //text sticks to arrow
+    backText.x = Math.floor(this.arrow.x + 8);
+    backText.y = Math.floor(this.arrow.y + 2);
+
+    fixedText.x = Math.floor(this.fixed.x - 8);
+    fixedText.y = Math.floor(this.fixed.y + 2);
+
+
   },
 
-  animateBikePart: function(part) {
+  writeBikeStats: function(part) {
+    console.log(part);
 
-    console.log(part.customParams.health + ' Percent!');
+    let obj = {
+      'distance': part.customParams.distance,
+      'health': part.customParams.health,
+      'lifespan':part.customParams.lifespan,
+      name:part.key
+    }
 
-    let distance = part.customParams.distance
-    let health = part.customParams.health
-
-    hpLabel = this.game.add.text(this.game.world.centerX - 100, 20, `Health: ${health}`, {
+    hpLabel = this.game.add.text(this.game.world.centerX - 150, 20, `Health: ${obj.health}`, {
+      font: '24px Arial',
+      fill: '#000'
+    })
+    distLabel = this.game.add.text(this.game.world.centerX + 50, 60, `Distance: ${obj.distance}`, {
+      font: '24px Arial',
+      fill: '#000'
+    })
+    lifespanLabel = this.game.add.text(this.game.world.centerX - 150, 60, `Wear: ${(obj.distance / obj.lifespan)*100}  %`, {
       font: '24px Arial',
       fill: '#000'
     })
 
-    distLabel = this.game.add.text(this.game.world.centerX - 100, 60, `Distance: ${distance}`, {
-      font: '24px Arial',
-      fill: '#000'
-    })
+    localStorage.setItem('partObj', JSON.stringify(obj))
+
   },
 
-  repair: function(part) {
-      hpLabel.text = 'Health: 100'
-      distLabel.text = `Distance: ${part.maxDistance}`
+  fixBikeStats: function() {
+
+    let partObj = JSON.parse(localStorage.getItem('partObj'))
+
+    partObj.distance = 0
+    partObj.health = 100
+
+    console.log(partObj)
+    //write new stats to db
+
+
   },
 
-  startBack: function(btn) {
+  back: function() {
+    hpLabel.destroy()
+    lifespanLabel.destroy()
+    distLabel.destroy()
+  },
+
+  start: function(btn, string, activePart) {
     btn.inputEnabled = true;
     btn.input.pixelPerfectClick = true;
-    btn.events.onInputDown.add(this.repair, this)
-  },
-
-  startInput: function(part) {
-    part.inputEnabled = true;
-    part.input.pixelPerfectClick = true;
-    part.events.onInputDown.add(this.animateBikePart, this)
+    if (string === 'back') btn.events.onInputDown.add(this.back, this)
+    if (string === 'input') btn.events.onInputDown.add(this.writeBikeStats, this)
+    if (string === 'fixed') btn.events.onInputDown.add(this.fixBikeStats, this)
   },
 
 };
