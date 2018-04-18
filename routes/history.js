a344cc490e23031fb1cefddb23dc0b453545336b
Mac Decker <mcdecke@gmail.com>
2018-04-18 16:51:00 -0600

    starting on settings

--- routes/history.js
+++ routes/history.js
@@ -2,7 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
+    console.log('hi!!!')
+
+  //local storage doesn't exist on the backend, need to use ajax to store the localStorage from the front end.
+  // let partObj = JSON.parse(localStorage.getItem('partObj'))
+  // console.log(partObj)
+
   res.render('history', { title: 'Bicycle Health' })
+
 })
 
 module.exports = router
--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+//grey out email -- not changeable
+//prepopulate all other fields
+
 router.get('/', (req, res, next) => {
   res.render('profile', { title: 'Bicycle Health' })
 })
