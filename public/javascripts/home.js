2f86b6610cd4adb14d275bac34ab73b846fb7e03
Mac Decker <mcdecke@gmail.com>
2018-04-18 08:44:09 -0600

    100% width iframe

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -6,7 +6,6 @@ var GameState = {
     this.load.image('Bike', '../images/bike-vector-full.png')
     this.load.image('Wheel', '../images/wheel.png')
     this.load.image('Brake', '../images/disc-brake-logo.png')
-
     this.load.image('Seat', '../images/seat.png')
     this.load.image('Chain', '../images/chain.png')
     this.load.image('Pedal', '../images/pedal.png')
@@ -26,7 +25,9 @@ var GameState = {
     //load bike
     this.bike = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'Bike')
     this.bike.anchor.setTo(0.5)
+
     //stats will be pulled in from db and written here
+    // this.bike customParams = db shenanigans here
     this.bike.customParams = {
       health: 99,
       distance: 1200,
@@ -67,6 +68,7 @@ var GameState = {
       lifespan: 15000
     }
     this.start(this.handlebars, 'input', this.handlebars)
+    this.healthGlow(this.handlebars)
 
     //load seat
     this.seat = this.game.add.sprite(this.game.world.centerX - 104, this.game.world.centerY - 150, 'Seat')
@@ -78,6 +80,7 @@ var GameState = {
       lifespan: 15000
     }
     this.start(this.seat, 'input', this.seat)
+    this.healthGlow(this.seat)
 
     //load chain
     this.chain = this.game.add.sprite(this.game.world.centerX - 124, this.game.world.centerY + 84, 'Chain')
@@ -89,6 +92,7 @@ var GameState = {
       lifespan: 15000
     }
     this.start(this.chain, 'input', this.chain)
+    this.healthGlow(this.chain)
 
     //load pedal
     this.pedal = this.game.add.sprite(this.game.world.centerX - 6, this.game.world.centerY + 144, 'Pedal')
@@ -162,21 +166,28 @@ var GameState = {
 
     localStorage.setItem('partObj', JSON.stringify(obj))
     this.getBikeStats()
+  },
 
+  healthGlow: function(part) {
+    console.log(part);
+    shadow = game.add.sprite(game.world.centerX, game.world.centerY, part.key);
+    shadow.anchor.set(0.5);
+    shadow.tint = '#46ff13';
+    shadow.alpha = 0.5;
   },
 
   getBikeStats: function() {
 
-        let partObj = JSON.parse(localStorage.getItem('partObj'))
+    let partObj = JSON.parse(localStorage.getItem('partObj'))
 
-        console.log(partObj.health)
-        console.log(partObj.distance)
-        console.log(partObj.lifespan)
-        console.log(partObj.name)
-        // //write new stats to db
-        const parts = parent.document.getElementById('partHealth')
-        console.log(parts)
-        $(parts).html(`Displaying: <b>${partObj.name}</b> <br> Distance since ${partObj.name} changed: ${partObj.distance} km. <br> Suggested Distance until next swap: ${partObj.lifespan - partObj.distance}`)
+    console.log(partObj.health)
+    console.log(partObj.distance)
+    console.log(partObj.lifespan)
+    console.log(partObj.name)
+    // //write new stats to db
+    let parts = parent.document.getElementById('partHealth')
+    console.log(parts)
+    $(parts).html(`Displaying: <b>${partObj.name}</b> <br> Distance since ${partObj.name} changed: ${partObj.distance} km. <br> Suggested Distance until next swap: ${partObj.lifespan - partObj.distance}`)
   },
 
   fixBikeStats: function() {
@@ -192,9 +203,9 @@ var GameState = {
     console.log(partObj.name)
 
     // //write new stats to db
-    // const parts = parent.document.getElementById('partHealth')
-    // console.log(parts+'!');
-    // $(parts).text(`Displaying: ${partObj.name} `+'\n'+` Distance since ${partObj.name} changed: ${partObj.distance} km. \n Suggested Distance until next swap: ${partObj.lifespan - partObj.distance}`)
+    let parts = parent.document.getElementById('partHealth')
+    console.log(parts)
+    $(parts).html(`Displaying: <b>${partObj.name}</b> <br> Distance since ${partObj.name} changed: ${partObj.distance} km. <br> Suggested Distance until next swap: ${partObj.lifespan - partObj.distance}`)
   },
 
   // back: function() {
