0361f1e89e5b7bfa5ec3c368fb1ec83689309c71
Tanya <mellene@gmail.com>
2018-04-19 15:10:49 -0600

    added check for empty repair history

--- routes/history.js
+++ routes/history.js
@@ -10,38 +10,53 @@ const RepairService = require('../database/services/repairService')
 router.get('/', verifyToken, retrieveUser, (req, res, next) => {
   console.log('GET: history page')
   console.log('For user: ', req.token.email)
-  console.log('headers', req.headers)
   const contype = req.headers['content-type']
   console.log('content:', contype)
   const bikeService = new BikeService()
   const repairService = new RepairService()
   bikeService.list(req.user.id)
     .then((bikeList) => {
+      if(bikeList.length === 0) {
+        return null
+      }
       return Promise.all(bikeList.map((bike) => repairService.listAll(bike.id)))
     })
     .then((repairs) => {
-      return repairs.map((repair, index) => {
-        return repair.reduce((acc, element) => {
-          if (acc.bike_name) {
-            acc.history.push(element)
-          }
-          else {
-            acc.bike_name = element.bike_nick_name
-            acc.history = []
-            acc.history.push(element)
-          }
-          return acc
-        }, {})
-      })
+      if(repairs) {
+        return repairs.map((repair, index) => {
+          return repair.reduce((acc, element) => {
+            if (acc.bike_name) {
+              acc.history.push(element)
+            }
+            else {
+              acc.bike_name = element.bike_nick_name
+              acc.history = []
+              acc.history.push(element)
+            }
+            return acc
+          }, {})
+        })
+      } else {
+        return null
+      }
     })
     .then((repairs) => {
-      if (contype && contype.indexOf('application/json') === 0) {
-        res.json({ history: repairs })
-      }
-      else {
+      if (repairs) {
+        if (contype && contype.indexOf('application/json') === 0) {
+          res.json({ history: repairs })
+        }
+        else {
+          res.render('history', {
+            title: APP_TITLE,
+            repairs: repairs,
+            text: ''
+          })
+        }
+      } else {
         res.render('history', {
           title: APP_TITLE,
-          repairs: repairs
+          repairs: [],
+          text: 'There are no repair records yet'
         })
       }
     })
